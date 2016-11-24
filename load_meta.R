b6defd92828d04a95a26116bb6fbdd810cabfcf5
James Hawley <jamesrichardhawley@gmail.com>
2016-11-24 12:41:33 -0500

    Refactored setup

--- load_meta.R
+++ load_meta.R
@@ -1,12 +1,33 @@
 #source("https://bioconductor.org/biocLite.R");
 #biocLite("affy");
 #biocLite("affxparser");
+#biocLite("affyio");
 
-library("affxparser");
-files <- c("GSE20711_RAW/GSM519722.CEL");
-header_meta <- strsplit(readCelHeader(files[[1]])$datheader, "\\s+")[[1]];
-date <- header_meta[[8]];
-time <- header_meta[[9]];
+### Environment ###############################################################
+library("affyio");
+library("data.table");
 
-date;
-time;
+### Functions #################################################################
+# list.dirs
+# Description:
+#   List basenames of directories in path
+# Inputs:
+#   path:           Path to list directories in (default: ".")
+#   pattern:        Name matching pattern (default: NULL)
+#   all.dirs:       List all directories or not (default: FALSE)
+#   full.names:     List full path names or just basenames (default: FALSE)
+#   ignore.case:    Ignore case or not for matching (default: FALSE)
+# Outputs:
+#   List of strings containing directory names
+list.dirs <- function(path=".", pattern=NULL, all.dirs=FALSE, full.names=FALSE, ignore.case=FALSE) {
+    # use full.names=TRUE to pass to file.info
+    all <- list.files(path, pattern, all.dirs, full.names=TRUE, recursive=FALSE, ignore.case);
+    dirs <- all[file.info(all)$isdir];
+
+    # determine whether to return full names or just dir names
+    if (isTRUE(full.names)) {
+        return(dirs)
+    } else {
+        return(basename(dirs))
+    }
+}
