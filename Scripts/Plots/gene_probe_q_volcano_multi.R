77ee3a9e1281e6db32eeb4b90647f9d6e79f0cab
James Hawley <jamesrichardhawley@gmail.com>
2016-12-14 14:23:44 -0500

    Plotting q-values for new MCF-7 dataset

---
+++ Scripts/Plots/gene_probe_q_volcano_multi.R
@@ -0,0 +1,37 @@
+# Scatterplot of median RMA-normalized values
+q_data <- data.frame();
+for (GSE in dbs) {
+    med_col <- apply(exprs(affy_dbs_rma[[GSE]]), 2, median);
+    q_data <- rbind.data.frame(
+        q_data,
+        cbind.data.frame(
+            "Slope" = regr_values_rma[[GSE]][,"coeff"],
+            "Q" = -log10(regr_values_rma[[GSE]][,"q"]),
+            "Dataset" = GSE
+        )
+    );
+}
+png("GPL570_rma_q.png", width = 3000, height = 1800, res = 300);
+p1 <- (
+    ggplot(
+        data = q_data,
+        mapping = aes(x = Slope, y = Q, colour = Dataset))
+    + geom_point(alpha = 0.2)
+    + geom_hline(yintercept = 2, linetype = 2)
+    + ggtitle("Significant probe expression")
+    + xlab("Linear regression slope")
+    + ylab("-log10(q) (FDR adjusted)")
+    + xlim(c(-1.5e-6, 1.5e-6))
+    + guides(colour = guide_legend(override.aes = list(alpha = 1)))
+    # + ylim(c(4,6))
+    # + scale_x_datetime(date_labels = "%Y-%m-%d", date_breaks = "4 month")
+    # + theme(
+    #     axis.text.x = element_text(angle = 90),
+    #     panel.background = element_rect(fill = "transparent",colour = NA), # or theme_blank()
+    #     panel.grid.minor = element_blank(), 
+    #     panel.grid.major = element_blank(),
+    #     plot.background = element_rect(fill = "transparent",colour = NA)
+    # )
+);
+print(p1);
+dev.off();
