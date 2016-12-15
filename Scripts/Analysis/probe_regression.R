5f6c46c015fb338a2c9916fece7e7c62ca6b28fb
James Hawley <jamesrichardhawley@gmail.com>
2016-12-15 18:40:31 -0500

    Removed MAS5 normalization
    Things seem pretty consistent across normalizations, so comparing RMA
    and MAS5 seems time consuming.
    
    RMA normalization is also much faster, so I'm sticking with that

--- Scripts/Analysis/probe_regression.R
+++ Scripts/Analysis/probe_regression.R
@@ -2,8 +2,6 @@
 print("Regression Analysis");
 # p-values of linear regression having no slope
 regr_values_rma <- list();
-regr_values_mas5 <- list();
-regr_log2_mas5 <- list();
 for (GSE in dbs) {
     print(GSE);
     # apply regression test to each row of matrix containing expression for a probe at the time it was taken
@@ -22,24 +20,6 @@ for (GSE in dbs) {
         p.adjust(regr_values_rma[[GSE]][,1], method = correction_method)
     );
     colnames(regr_values_rma[[GSE]]) <- c("p", "coeff", "q");
-
-    # apply regression test to each row of matrix containing expression for a probe at the time it was taken
-    regr_log2_mas5[[GSE]] <- t(apply(
-        exprs(affy_dbs_mas5[[GSE]]),
-        1,
-        function (a) {
-            probe_lm <- lm(log2(a) ~ cel_datetimes[[GSE]]); # log2(expression data), making it more comparable to RMA 
-            gene_summary <- summary.lm(probe_lm);
-            return(c(gene_summary$coefficients[2,4], gene_summary$coefficients[2,1]));
-        }
-    ));
-    # q-values after FDR correction
-    regr_log2_mas5[[GSE]] <- cbind(
-        regr_log2_mas5[[GSE]],
-        p.adjust(regr_log2_mas5[[GSE]][,1],
-        method = correction_method)
-    );
-    colnames(regr_log2_mas5[[GSE]]) <- c("p", "coeff", "q");
 }
 
 ### Map significant genes #######################
@@ -56,23 +36,3 @@ sig_genes_rma <- sapply(
         )));
     }
 );
-sig_genes_log2_mas5 <- sapply(
-    dbs,
-    function (GSE) {
-        return(sum(sapply(
-            gene_names,
-            function (gene) {
-                return(regr_log2_mas5[[GSE]][gene, "q"] < significance);
-            }
-        )));
-    }
-);
-
-# get info for probes that are consistently significant
-probe_info <- t(sapply(
-    # names of probes that are significant in 4 of the 5 datasets
-    names(which(sig_genes_rma == 4)),
-    function (probe_id) {
-        return(hgu_probe_map[which(hgu_probe_map[,"PROBEID"] == probe_id),-1]);
-    }
-));
--- Scripts/Plots/gene_probe_q_volcano.R
+++ Scripts/Plots/gene_probe_q_volcano.R
@@ -9,16 +9,7 @@ xlims_rma <- list(
     "GSE8141" = 2.5e-7,
     "GSE8565" = 6e-7
 )
-xlims_log2_mas5 <- list(
-    "GSE6800" = 4e-7,
-    "GSE6803" = 4e-7,
-    "GSE7161" = 1.5e-3,
-    "GSE7327" = 1.5e-6,
-    "GSE8139" = 2.7e-7,
-    "GSE8140" = 1.5e-6,
-    "GSE8141" = 3.5e-7,
-    "GSE8565" = 8e-7
-);
+
 for (GSE in dbs) {
     png(
         paste("GPL570_volcano_RMA_", GSE, ".png", sep = ""),
@@ -29,24 +20,7 @@ for (GSE in dbs) {
     plot(
         x = regr_values_rma[[GSE]][,"coeff"],
         y = -log10(regr_values_rma[[GSE]][,"q"]),
-        xlim = c(-xlims_rma[[GSE]], xlims_rma[[GSE]]),
-        pch = ".",
-        xlab = "Regression coefficient",
-        ylab = "-log10(q) (FDR adjusted)"
-    );
-    abline(h = 2, lty = 2);
-    dev.off()
-
-    png(
-        paste("GPL570_volcano_log2MAS5_", GSE, ".png", sep = ""),
-        width = 3000,
-        height = 1800,
-        res = 300
-    );
-    plot(
-        x = regr_log2_mas5[[GSE]][,"coeff"],
-        y = -log10(regr_log2_mas5[[GSE]][,"q"]),
-        xlim = c(-xlims_log2_mas5[[GSE]], xlims_log2_mas5[[GSE]]),
+        # xlim = c(-xlims_rma[[GSE]], xlims_rma[[GSE]]),
         pch = ".",
         xlab = "Regression coefficient",
         ylab = "-log10(q) (FDR adjusted)"
