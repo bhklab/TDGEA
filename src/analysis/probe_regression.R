1d6cf927059c0e5247c9d13aac8efefd356598aa
James Hawley <jamesrichardhawley@gmail.com>
2016-12-21 11:27:48 -0500

    Refactoring regression with nested applies

--- src/analysis/probe_regression.R
+++ src/analysis/probe_regression.R
@@ -3,23 +3,28 @@ correction_method <- "fdr";
 
 print("Regression Analysis");
 # p-values of linear regression having no slope
-regr_values_rma <- list();
-for (GSE in dbs) {
-    print(GSE);
-    # apply regression test to each row of matrix containing expression for a probe at the time it was taken
-    regr_values_rma[[GSE]] <- t(apply(
-        exprs(affy_dbs_rma[[GSE]]),
-        1,
-        function (a) {
-            probe_lm <- lm(a ~ cel_datetimes[[GSE]]);
-            gene_summary <- summary.lm(probe_lm);
-            return(c(gene_summary$coefficients[2,4], gene_summary$coefficients[2,1]));
-        }
-    ));
-    # q-values after FDR correction
-    regr_values_rma[[GSE]] <- cbind(
-        regr_values_rma[[GSE]],
-        p.adjust(regr_values_rma[[GSE]][,1], method = correction_method)
-    );
-    colnames(regr_values_rma[[GSE]]) <- c("p", "coeff", "q");
-}
+regr_values_rma <- sapply(
+    dbs,
+    function (GSE) {
+        print(GSE);
+        # apply regression test to each row of matrix containing expression for a probe at the time it was taken
+        regr_columns <- t(apply(
+            exprs(affy_dbs_rma[[GSE]]),
+            1,
+            function (a) {
+                probe_lm <- lm(a ~ cel_datetimes[[GSE]]);
+                gene_summary <- summary.lm(probe_lm);
+                return(c(gene_summary$coefficients[2,4], gene_summary$coefficients[2,1]));
+            }
+        ));
+        # q-values after FDR correction
+        regr_columns <- cbind(
+            regr_columns,
+            p.adjust(regr_columns[,1], method = correction_method)
+        );
+        colnames(regr_columns) <- c("p", "coeff", "q");
+
+        return(regr_columns);
+    },
+    simplify = FALSE
+);
